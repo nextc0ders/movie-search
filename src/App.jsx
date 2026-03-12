0c6aed2105c548dd65939b45692ff99b4475f563
418ERT <418ert.dev@gmail.com>
2026-03-12 15:52:23 +0400

    2 chapter. Сделали карточки товаров, пагинацию и обновили дизайн

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
+const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+
 function App() {
+	const [page, setPage] = useState(1);
 	const [movie, setMovie] = useState([]);
 	const [query, setQuery] = useState('');
+	const [debouncedQuery, setDebouncedQuery] = useState('');
+
 	const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 	useEffect(() => {
+		const handler = setTimeout(() => {
+			setDebouncedQuery(query);
+			setPage(1);
+		}, 500)
 
-		const url = query
-			? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
-			: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`;
+		return () => clearTimeout(handler);
+	}, [query]);
+
+
+	useEffect(() => {
+		const url = debouncedQuery
+			? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${debouncedQuery}&page=${page}`
+			: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`;
 
 		fetch(url)
 			.then(res => res.json())
@@ -20,15 +34,18 @@ function App() {
 				}
 			})
 			.catch(err => console.error(err))
-	}, [query, API_KEY]);
+	}, [debouncedQuery, page, API_KEY]);
 
 	return (
-		<div className="app">
+		<div className="app-container">
 
 			<header>
 				<h1 className="title">Movie Search</h1>
 			</header>
 
+			{/* TODO
+				нужно сделать очистку поиска
+			*/}
 			<input
 				type="text"
 				className="search-input"
@@ -39,10 +56,32 @@ function App() {
 			<ul className="movie-list">
 				{movie.map((m) => (
 					<li key={m.id} className="movie-item">
-						{m.title}
+						<h3>{m.title}</h3>
+						<img
+							src={m.poster_path ? IMG_URL + m.poster_path : "https://placehold.co/200x300"}
+							alt={m.title}
+						/>
 					</li>
 				))}
 			</ul>
+
+			<div className="pagination">
+				<button
+					disabled={page === 1}
+					onClick={() => setPage(prev => prev - 1)}
+				>
+					Назад
+				</button>
+
+				<span>Старница {page}</span>
+
+				<button
+					onClick={() => setPage(prev => prev + 1)}
+				>
+					Вперед
+				</button>
+			</div>
+
 		</div>
 	)
 }
