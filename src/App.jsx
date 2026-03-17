7281c6dcdefefb2350a9e40a77c8dbc2fdfb1890
418ERT <418ert.dev@gmail.com>
2026-03-17 16:36:40 +0400

    3 chapter. Создали шапку сайта и разделили контент на несколько частей

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 	const [movie, setMovie] = useState([]);
 	const [query, setQuery] = useState('');
 	const [debouncedQuery, setDebouncedQuery] = useState('');
+	const [contentType, setContentType] = useState('movie');
 
 	const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
@@ -23,8 +24,8 @@ function App() {
 
 	useEffect(() => {
 		const url = debouncedQuery
-			? `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${debouncedQuery}&page=${page}`
-			: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`;
+			? `https://api.themoviedb.org/3/search/${contentType}?api_key=${API_KEY}&query=${debouncedQuery}&page=${page}`
+			: `https://api.themoviedb.org/3/${contentType}/popular?api_key=${API_KEY}&page=${page}`;
 
 		fetch(url)
 			.then(res => res.json())
@@ -34,58 +35,97 @@ function App() {
 				}
 			})
 			.catch(err => console.error(err))
-	}, [debouncedQuery, page, API_KEY]);
+	}, [debouncedQuery, page, contentType, API_KEY]);
 
 	return (
-		<div className="app-container">
-
-			<header>
+		<>
+			<header className='header'>
 				{/* TODO
 					нужно сгенерировать логотип и сделать роутинг на главную страницу
 				*/}
-				<h1 className="title">Movie Search</h1>
+				<div className="logo title">Movie Search</div>
+				<nav className="nav">
+					<div className="dropdown">
+						<button className="dropbtn">
+							{contentType === 'movie' ? 'Фильмы' : 'Сериалы'} ▼
+						</button>
+						<div className="division"></div>
+						<div className="dropdown-content">
+							
+							<a href="#" onClick={() => {
+									setContentType('movie');
+									setPage(1);
+									setQuery('');
+							}}>Фильмы</a>
+
+							<a href="#" onClick={() => {
+									setContentType('tv');
+									setPage(1);
+									setQuery('');
+							}}>Сериалы</a>
+
+						</div>
+					</div>
+				</nav>
+
+				<div className="dropdown">
+					<button className="dropbtn">🌐</button>
+					<div className="division"></div>
+					<div className="dropdown-content lang">
+						<a href="#" onClick={() => {
+									
+							}}>EN</a>
+
+							<a href="#" onClick={() => {
+									
+							}}>RU</a>
+					</div>
+				</div>
+
 			</header>
 
-			{/* TODO
-				нужно сделать очистку поиска
-			*/}
-			<input
-				type="text"
-				className="search-input"
-				placeholder="Поиск фильмов..."
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
-			<ul className="movie-list">
-				{movie.map((m) => (
-					<li key={m.id} className="movie-item">
-						<h3>{m.title}</h3>
-						<img
-							src={m.poster_path ? IMG_URL + m.poster_path : "https://placehold.co/200x300"}
-							alt={m.title}
-						/>
-					</li>
-				))}
-			</ul>
+			<div className="app-container">
+				{/* TODO
+					нужно сделать очистку поиска
+				*/}
+				<input
+					type="text"
+					className="search-input"
+					placeholder="Поиск фильмов..."
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+				/>
+				<ul className="movie-list">
+					{movie.map((m) => (
+						<li key={m.id} className="movie-item">
+							<h3>{m.title || m.name}</h3>
+							<img
+								src={m.poster_path ? IMG_URL + m.poster_path : "https://placehold.co/200x300"}
+								alt={m.title || m.name}
+							/>
+						</li>
+					))}
+				</ul>
 
-			<div className="pagination">
-				<button
-					disabled={page === 1}
-					onClick={() => setPage(prev => prev - 1)}
-				>
-					Назад
-				</button>
+				<div className="pagination">
+					<button
+						disabled={page === 1}
+						onClick={() => setPage(prev => prev - 1)}
+					>
+						Назад
+					</button>
 
-				<span>Старница {page}</span>
+					<span>Старница {page}</span>
 
-				<button
-					onClick={() => setPage(prev => prev + 1)}
-				>
-					Вперед
-				</button>
+					<button
+						onClick={() => setPage(prev => prev + 1)}
+					>
+						Вперед
+					</button>
+				</div>
+
 			</div>
-
-		</div>
+		</>
 	)
 }
 
